5d78c832feb4eaf014a9e6869be2b733a5167e50
MichaelK <96792007+MajklKra@users.noreply.github.com>
2024-08-06 23:21:00 +0200

    Commit Init

---
+++ test.js
@@ -0,0 +1,10 @@
+/* 
+document.getElementById('me').onmouseover = function()
+{
+    this.style.color = 'white';
+};
+
+document.getElementById('me').onmouseleave = function()
+{
+    this.style.color = '#94a3b8';
+}; */
